5ecb55556db5b84ace1b88445c91a12ec480e24c
yarik <super.racing@ukr.net>
2025-06-19 14:51:51 +0300

    and another one

--- app/main.py
+++ app/main.py
@@ -12,4 +12,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, item) for key, item in linter_report.items()]
+    return [format_single_linter_file(key, item)
+            for key, item in linter_report.items()]
