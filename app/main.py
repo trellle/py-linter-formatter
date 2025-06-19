688b523a524aaf14633fe30210226a2ae206aaaa
yarik <super.racing@ukr.net>
2025-06-19 13:28:18 +0300

    and another one

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {"errors": [format_linter_error(error) for error in errors],
-            "path": file_path, "status": "pass" if not errors else "failed"}
+            "path": file_path, "status": "passed" if not errors else "failed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
