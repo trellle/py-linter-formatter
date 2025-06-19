bda7cbd5083d3048c495b0dcfe999289e929ed1e
yarik <super.racing@ukr.net>
2025-06-19 13:17:10 +0300

    another commit

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def format_linter_error(error: dict) -> dict:
-    return {"line": error["line-number"],
+    return {"line": error["line_number"],
             "column": error["column_number"],
             "message": error["text"],
             "name": error["code"],
