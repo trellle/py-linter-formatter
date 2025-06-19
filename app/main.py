4a89e1ef93c7f2fa4c27df771c6ad6146a0f8484
yarik <super.racing@ukr.net>
2025-06-19 13:12:57 +0300

    deleted comments

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
     return {"line": error["line-number"],
             "column": error["column_number"],
             "message": error["text"],
@@ -8,13 +7,11 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
     return {"errors": [format_linter_error(error) for error in errors],
             "path": file_path, "status": "pass" if not errors else "failed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
     return [format_single_linter_file(list(linter_report.keys())[0], []),
             format_single_linter_file(list(linter_report.keys())[1],
             linter_report[list(linter_report.keys())[1]])]
